c9e4e90d36f3afa21503b8df86c628716bc6e8a5
kambae <ryanwp526@gmail.com>
2019-06-19 10:57:05 +1000

    insert div code

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,3 +1,7 @@
-var div=document.createElement("div"); 
-document.body.appendChild(div); 
-div.innerText="test123";
+var div = document.createElement("div"); 
+var footer_array = document.getElementsByTagName('footer');
+if (!footer_array.length == 0) {
+	var footer = footer_array[footer_array.length - 1];
+	footer.parentNode.insertBefore(div, footer);
+}
+div.innerText = "test123";
