c98e44e0e30b67a7319fe1f3fb9f272b061027b9
kambae <ryanwp526@gmail.com>
2019-06-19 11:02:48 +1000

    remove insert, add popup

--- chrome_extension/content.js
+++
@@ -1,7 +0,0 @@
-var div = document.createElement("div"); 
-var footer_array = document.getElementsByTagName('footer');
-if (!footer_array.length == 0) {
-	var footer = footer_array[footer_array.length - 1];
-	footer.parentNode.insertBefore(div, footer);
-}
-div.innerText = "test123";
