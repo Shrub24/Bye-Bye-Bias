d06b12dae2065e259a3a61bd05ef32ab7d392028
kambae <ryanwp526@gmail.com>
2019-06-21 13:42:22 +1000

    server works
    returns strength and similar articles from a localhost server using HTTP GET and extension displays

--- chrome_extension/popup_js.js
+++ chrome_extension/popup_js.js
@@ -1,21 +1,31 @@
 var source_url = "http://localhost:8000"
 
-function get_info(url){
+function update_info(url){
 	var xmlHttp = new XMLHttpRequest();
 	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     	current_url = tabs[0].url
-    	alert(current_url)
     	xmlHttp.open("GET", url + "?" + current_url, false);
 		xmlHttp.send();
 		// alert(xmlHttp.responseText)
-		return xmlHttp.responseText
+		info = JSON.parse(xmlHttp.responseText)
+		// alert(info["topic"])
+		document.getElementById("topic").innerHTML = info["topic"]
+		document.getElementById("strength").innerHTML = info["strength"]
+		document.getElementById("articles").innerHTML = create_dual_html_list(info["articles"])
 	});
 }
 
-function main(){
-	get_info(source_url)
+function create_dual_html_list(dict){
+	var str = "<ul>"
+	keys = Object.keys(dict)
+	keys.forEach(function(key){
+		str += "<li>" + key + " " + dict[key]
+	})
+
+	str += "</ul>";
+	return str
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  main()
+  update_info(source_url)
 });
--- server/db_server.py
+++ server/db_server.py
@@ -1,26 +1,39 @@
 import mysql.connector
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
+import json
 
 PORT_NUMBER = 8000
+
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         query = urlparse(self.path).query
         print(query)
         # self.send_response(200)
-        # self.wfile.write("hiii")
+        response = get_strength_and_similar(query)
+        print(response)
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+
+        self.wfile.write(json.dumps(response).encode())
         return
 
 
 def get_strength_and_similar(url):
     cur = db.cursor()
     cur.execute("SELECT strength, topic FROM articles WHERE url='" + url + "'")
+    # todo create if no result condition
     result = cur.fetchall()[0]
     strength = result[0]
     topic = result[1]
     cur.execute("SELECT url, strength FROM articles WHERE topic='" + topic + "'")
-    return (topic, strength, cur.fetchall())
+    articles = cur.fetchall()
+    return {"topic": topic, "strength": strength, "articles": {str(i[0]): i[1] for i in articles}}
+
 
 if __name__ == "__main__":
     db = mysql.connector.connect(host="localhost", user="byebyebias", passwd="bias123", db="articles")
