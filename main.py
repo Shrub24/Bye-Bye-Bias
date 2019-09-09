af4668246455a844b93ee9a6dc3ebd459d05edae
kambae <ryanwp526@gmail.com>
2019-09-09 16:18:58 +1000

    add embedding

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from news_sentiment import doc_sentiment_analysis
-from news_sentiment import cnn
+from news_sentiment.doc_sentiment_analysis import *
+from news_sentiment.cnn import *
 import populate_database
 from preprocessing.article_entities import entity_getter
 
 if __name__ == "__main__":
     net = load_model("models\\cnn3.pkl")
+    embedding = load_embedding("embedding_model\\glove.twitter.27B.100d")
     URL_PATH = "urls.txt"
     NUM_MAIN_ENTITIES = 3
     entity_getter_instance = entity_getter()
@@ -12,5 +13,5 @@ if __name__ == "__main__":
         url_list = url_file.readlines()
     raw_infos = get_scraped_infos(url_list)
     raw_texts = [info["text"] for info in raw_infos]
-    entity_sentiments = [{entity: get_doc_sentiment(entity_getter_instance.get_sentence_target_lists(text, entity), net) for entity in entity_getter_instance.get_n_important_entities(text, NUM_MAIN_ENTITIES)} for text in raw_texts]
+    entity_sentiments = [{entity: get_doc_sentiment(entity_getter_instance.get_sentence_target_lists(text, entity), net, embedding) for entity in entity_getter_instance.get_n_important_entities(text, NUM_MAIN_ENTITIES)} for text in raw_texts]
     populate_database(generate_article_infos(raw_infos, entity_sentiments, get_all_entities(raw_texts, entity_getter_instance)))
