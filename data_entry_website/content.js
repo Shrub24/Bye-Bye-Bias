9ee87bddb3882ba50b1b4ad07b8a419748f51cf9
kambae <ryanwp526@gmail.com>
2019-11-30 02:38:56 +1100

    replaced examples with sample article
    data entry complete

--- data_entry_website/content.js
+++ data_entry_website/content.js
@@ -1,33 +1,50 @@
 var sentiments;
 var exampleNum = 0;
+// todo dont use global variable pass textID into generate HTML
+var e_id = 0;
 
 //Hardcoded examples
-var examples = []
+// var examples = []
+//
+// var entities = [[0, 3], [4, 7]]
+// var text = "the car is back"
+// examples.push([text, entities])
+//
+// var text = "this hole is very fun"
+// var entities = [[5, 9], [13, 17]]
+// examples.push([text, entities])
+//
+// var text = "Trump isn't a great guy"
+// var entities = [[0,5],[20, 23]]
+// examples.push([text, entities])
+//
+// var text = "Obama is very cool"
+// var entities = [[0, 5]]
+// examples.push([text, entities])
+//
+// var text = "He's just Biden his time"
+// var entities = [[0, 4], [10, 15]]
+// examples.push([text, entities])
+var example1text = "Trump is a really great guy. His wall has been very effective. His policies have benefited America greatly. Sed arcu non odio euismod lacinia at quis risus sed. Aliquet enim tortor at auctor urna nunc id cursus. Pretium aenean pharetra magna ac placerat. Nunc vel risus commodo viverra maecenas accumsan lacus. Sed nisi lacus sed viverra tellus. Quis risus sed vulputate odio ut. Commodo odio aenean sed adipiscing diam donec adipiscing. Faucibus purus in massa tempor nec feugiat. Consectetur purus ut faucibus pulvinar elementum integer enim.\n" +
+    "\n" +
+    "Leo a diam sollicitudin tempor. Varius duis at consectetur lorem. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Volutpat odio facilisis mauris sit amet massa vitae tortor. Tellus cras adipiscing enim eu turpis egestas pretium."
 
-var entities = [[0, 3], [4, 7]]
-var text = "the car is back"
-examples.push([text, entities])
+var example1entities = [[0, 5], [29, 37], [63, 75], [91, 98]]
 
-var text = "this hole is very fun"
-var entities = [[5, 9], [13, 17]]
-examples.push([text, entities])
+var example2text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis bibendum ut tristique et. Ullamcorper malesuada proin libero nunc consequat interdum. Neque aliquam vestibulum morbi blandit cursus risus. Integer vitae justo eget magna fermentum iaculis eu. Tellus integer feugiat scelerisque varius morbi enim nunc faucibus a. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Scelerisque varius morbi enim nunc faucibus. Platea dictumst quisque sagittis purus sit. Massa massa ultricies mi quis hendrerit dolor. Id aliquet lectus proin nibh nisl condimentum id venenatis a. Etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus.\n" +
+    "\n" +
+    "Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Aliquam sem et tortor consequat. Senectus et netus et malesuada fames ac. Id semper risus in hendrerit. Volutpat sed cras ornare arcu dui vivamus. Velit scelerisque in dictum non consectetur a erat nam at. Risus ultricies tristique nulla aliquet enim tortor at. Convallis posuere morbi leo urna. Donec adipiscing tristique risus nec feugiat in fermentum. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis.\n" +
+    "\n" +
+    "Tellus mauris a diam maecenas sed enim. Et leo duis ut diam quam nulla porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada. Eget gravida cum sociis natoque penatibus et magnis. Nisl tincidunt eget nullam non nisi est sit amet facilisis. Sit amet nisl suscipit adipiscing. Lobortis feugiat vivamus at augue eget arcu dictum. Nam at lectus urna duis convallis. Diam sollicitudin tempor id eu nisl nunc mi. Nunc lobortis mattis aliquam faucibus purus in massa. Eu mi bibendum neque egestas congue quisque. Faucibus turpis in eu mi bibendum neque egestas congue quisque. Vestibulum rhoncus est pellentesque elit. Magna sit amet purus gravida quis blandit turpis cursus in. Massa tempor nec feugiat nisl pretium fusce id. Ac turpis egestas integer eget aliquet nibh praesent tristique. Vitae auctor eu augue ut lectus arcu bibendum at varius. The world is a better place because of Mr Xi and his government."
 
-var text = "Trump isn't a great guy"
-var entities = [[0,5],[20, 23]]
-examples.push([text, entities])
-
-var text = "Obama is very cool"
-var entities = [[0, 5]]
-examples.push([text, entities])
-
-var text = "He's just Biden his time"
-var entities = [[0, 4], [10, 15]]
-examples.push([text, entities])
+var example2entities = [[12, 17], [400, 405], [2126, 2131], [2136, 2150]]
 
 window.onload = function() {
     var example_element = document.getElementById("example_text");
-    firstExample = getNextExample()
-    example_element.innerHTML = generateExampleHTML(firstExample[1], firstExample[0])
+    // firstExample = getNextExample()
+    example_element.innerHTML = generateExampleHTML(example1entities, example1text);
+
+    document.getElementById("example_text2").innerHTML = generateExampleHTML(example2entities, example2text);
 }
 
 //entities is array of ORDERED character indexes (start-inclusive, end-exclusive)
@@ -35,7 +52,6 @@ function generateExampleHTML(entities, text) {
     sentiments = Array(entities.length).fill("n");
     var html = text;
     var offset = 0;
-    var e_id = 0
     for(var entity of entities) {
         var startIndex = entity[0];
         var endIndex = entity[1];
