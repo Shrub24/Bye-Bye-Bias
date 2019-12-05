cc286913fb6fc218acabbdf24477ec0c74eddb4d
wakass222 <will-z@live.com.au>
2019-12-06 00:22:09 +1100

    Revert "Merge branch 'analytics'"
    This reverts commit 2b2cd861d74abfb51fcbe9f3e26f85cc1036ff35, reversing
    changes made to 8ee20d950157f4d4ac0425a63166263be55aeabe.

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -2,53 +2,50 @@ var serverUrl = "http://localhost:8000/"
 
 function fetchUrlAndStore(urlToFetch, tabId, callback) {
   localStorage.setItem(urlToFetch, "fetching");
-
-  //Determine unique user ID
-  chrome.identity.getProfileUserInfo(function(userInfo) {
-    fetch(serverUrl + "?url=" + urlToFetch + "&id=" + userInfo.id, {method: "GET"}).then(function (response) {
-      if (!response.ok) {
-        return false
-      }
-      return response.text();
-    })
-        .then(function (body) {
-          if (body == "unknown") {
-            var currentDate = new Date();
-            localStorage.setItem(urlToFetch, JSON.stringify({"unknown": true, Date: currentDate}));
-          } else if (body) {
-            body.day = new Date();
-            console.log(body);
-            localStorage.setItem(urlToFetch, body);
-          } else {
-            // Request error
-          }
-
-          var filtered = Object.keys(localStorage).filter(function (value) {
-            try {
-              return JSON.parse(localStorage.getItem(value)).hasOwnProperty("date");
-            } catch (error) {
-              return false;
-            }
-          });
-
-          if (filtered.length > 500) {
-            if (filtered != []) {
-              var oldest = filtered.reduce(function (acc, val) {
-                return JSON.parse(localStorage.getItem(acc)).date < JSON.parse(localStorage.getItem(val)).date ? acc : val;
-              });
-              localStorage.removeItem(oldest);
-            }
-          }
-
-          if (callback != undefined)
-            callback(urlToFetch, tabId);
-        })
-        .catch(function (err) {
-          // No server
-          localStorage.removeItem(urlToFetch);
-          throw err;
-        });
+  
+  fetch(serverUrl + "?" + urlToFetch, {method: "GET"}).then(function (response) {
+    if (!response.ok) {
+      return false
+    }
+    return response.text();
   })
+  .then(function(body) {
+    if (body == "unknown") {
+      var currentDate = new Date();
+      localStorage.setItem(urlToFetch, JSON.stringify({"unknown":true, Date: currentDate}));
+    } else if (body) {
+      body.day = new Date();
+      console.log(body);
+      localStorage.setItem(urlToFetch, body);
+    } else {
+      // Request error
+    }
+
+    var filtered = Object.keys(localStorage).filter(function (value) {
+      try {
+        return JSON.parse(localStorage.getItem(value)).hasOwnProperty("date");
+      } catch (error) {
+        return false;
+      }
+    });
+    
+    if(filtered.length > 500) {
+      if(filtered != []) {
+        var oldest = filtered.reduce(function(acc, val) {
+          return JSON.parse(localStorage.getItem(acc)).date < JSON.parse(localStorage.getItem(val)).date ? acc : val;
+        });
+        localStorage.removeItem(oldest);
+      }
+    }
+    
+    if(callback != undefined)
+      callback(urlToFetch, tabId);
+  })
+  .catch(function(err) {
+    // No server
+    localStorage.removeItem(urlToFetch);
+    throw err;
+  });
 }
 
 // Listen for any changes to the URL of any tab.
